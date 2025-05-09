b56fdc03df6fa6d56932e2f717bff0c0dae72476
elianhardyy <elianhardi805@gmail.com>
2025-05-09 16:29:36 +0700

    [IMPROVE] combine all configuration from many features to main

---
+++ src/app.module.ts
@@ -0,0 +1,62 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { RedisModule } from './redis/redis.module';
+import { CustomerModule } from './customer/customer.module';
+import { AwsS3Module } from './aws-s3/aws-s3.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { CommonResponseInterceptor } from './interceptor/common-response.interceptor';
+import { HttpExceptionFilter } from './api/response/http-exception.filter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get('DB_USERNAME', 'postgres'),
+        password: configService.get('DB_PASSWORD', 'postgres'),
+        database: configService.get('DB_NAME', 'nestecommerce'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    MulterModule.register({
+      limits: {
+        fileSize: 10 * 1024 * 1024, //10mb
+      },
+    }),
+    UserModule,
+    CategoryModule,
+    ProductModule,
+    CartModule,
+    RedisModule,
+    CustomerModule,
+    AwsS3Module,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CommonResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
