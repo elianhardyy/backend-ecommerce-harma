7265cad5c25fe5ee402d9d9f2dd24cefb8f0891c
elianhardyy <elianhardi805@gmail.com>
2025-05-09 16:29:53 +0700

    [FEATURE] application utility

---
+++ src/utils/DateUtil.ts
@@ -0,0 +1,21 @@
+export const parseDate = (date: any) =>
+  new Date(date).toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+export const formatDateFromInput = (input: any) => {
+  const date = new Date(input);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
