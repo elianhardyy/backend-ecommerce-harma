7786e643e14f03bc44ad09d4f56c25c10392c686
elianhardyy <elianhardi805@gmail.com>
2025-05-09 16:24:30 +0700

    [FEATURE] authorization guard

---
+++ src/guard/roles.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorator/roles.decorator';
+import { User } from 'src/user/entities/user.entity';
+import { UserRole } from 'src/user/enums/user.role';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user }: { user: User } = context.switchToHttp().getRequest();
+    return requiredRoles.some((role) => user.role.includes(role));
+  }
+}
